a694b331bde48f10bdaf38927ce349de5994c0eb
simphiwe zondi <simphiwez@bunkai.co.za>
2026-04-10 09:12:41 +0200

    adding jenkins file

--- src/main/java/co/za/test/features/TestFeaturesApplication.java
+++ src/main/java/co/za/test/features/TestFeaturesApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestFeaturessApplication {
+public class TestFeaturesApplication
+{
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestFeaturessApplication.class, args);
+		SpringApplication.run(TestFeaturesApplication.class, args);
 	}
 
 }
