f083e726d68012bd26f57f897c5dc427578a9c04
simphiwe zondi <simphiwez@bunkai.co.za>
2026-04-10 09:07:49 +0200

    initial project

---
+++ src/main/java/co/za/test/features/TestFeaturessApplication.java
@@ -0,0 +1,13 @@
+package co.za.test.features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFeaturessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFeaturessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/test/features/TestFeaturessApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.test.features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFeaturessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
